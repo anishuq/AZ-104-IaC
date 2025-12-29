e8fe7fba18204aaeb07849175f39ff9bdee335e8
unknown <dodulhuq@gmail.com>
2025-12-29 02:38:53 -0500

    vnetpeering corrected

--- vnetpeering-new.ps1
+++ vnetpeering-new.ps1
@@ -1,3 +1,8 @@
+<#
+This script creates two virtual networks in different regions and a jumpbox VM in one of the VNETs.
+This is all in preparation for VNET peering. Though the peering itself is not created in this script.
+#>
+
 #create a new VNET
 $ResourceGroupName = "vnetpeering-rg"
 
@@ -19,7 +24,18 @@ $SubnetAddressPrefix2 = "192.168.0.0/24"
 
 #creating jumpbox VM in vnet-eus-01  with public IP.
 $Vmname1 = "jumpbox-vm-01"
-$Image = "Ubuntu22LTS"
+$Vmname2 = "vnet-eus-01-vm-01"  #without public IP
+$Vmname3 = "vnet-wus-01-vm-01"  #without public IP
+$Image = "Canonical:0001-com-ubuntu-server-jammy:22_04-lts:latest"
+
+<#Username: admanisulhuq
+Password: McIe@4:5WmFvM
+Though this is not a secure password, this is just for lab purpose.
+#>
+$username = "admanisulhuq" #enter username for all VM
+$plainPassword = "McIe@4:5WmFvM" #enter password for VM
+$password = ConvertTo-SecureString $plainPassword -AsPlainText -Force
+$vmcred = New-Object System.Management.Automation.PSCredential ($username, $password)
 
 
 Connect-AzAccount
@@ -84,31 +100,77 @@ Write-Host "Virtual Network deployed successfully!"
 function Create-VMs {
     param (
         [string]$pipName,
+        [boolean]$EnablePublicIP,
         [string]$Vmname,
         [string]$VNetName,
         [string]$SubnetName,
         [string]$ResourceGroupName,
         [string]$Location,
-        [string]$AddressPrefix
+        [string]$Image
     )
-$pip-jumpbox-vm-01 = New-AzPublicIpAddress -Name $pipName `
+
+
+#Common VM parameters for all VMs.
+$vmParams = @{
+    ResourceGroupName  = $ResourceGroupName
+    Name               = $Vmname
+    Location           = $Location
+    Size               = "Standard_B1s"
+    VirtualNetworkName = $VNetName
+    SubnetName         = $SubnetName
+    Image              = $Image
+    Credential         = $vmcred
+}
+
+if ($EnablePublicIP) {
+    Write-Host "Public IP enabled. Creating PIP and adding to configuration..." -ForegroundColor Cyan
+    $pip = New-AzPublicIpAddress -Name $pipName `
          -ResourceGroupName $ResourceGroupName `
          -Location $Location `
          -AllocationMethod Static `
          -Sku Basic
 
+    $vmParams['PublicIpAddressName'] = $pip.Name
+    $vmParams["OpenPorts"] = 22
+}
 
-
-New-AzVM -ResourceGroupName $ResourceGroupName `
-         -Name $Vmname `
-         -Location $Location `
-         -Size "Standard_B1s" `
-         -VirtualNetworkName $VNetName `
-         -SubnetName $SubnetName `
-         -PublicIpAddressName $pip-jumpbox-vm-01.Name `
-         -OpenPorts 22 `
-         -Image $Image `
-         -Credential (Get-Credential)
+else {
+    Write-Host "Public IP not needed. Skipping..." -ForegroundColor Yellow
+    # You can leave this empty, or add settings specific to private VMs
+}
+New-AzVM @vmParams
 }
 
 $JBpipName = "pip-jumpbox-vm-01"
+#This is the public IP for the jumpbox VM.
+Create-VMs -pipName $JBpipName `
+           -EnablePublicIP $true `
+           -Vmname $Vmname1 `
+           -VNetName $VNetName1 `
+           -SubnetName $SubnetName1 `
+           -ResourceGroupName $ResourceGroupName `
+           -Location $Location1 `
+           -Image $Image
+        
+#Creating VM in vnet-eus-01 without public IP
+
+Create-VMs -pipName "" `
+           -EnablePublicIP $false `
+           -Vmname $Vmname2 `
+           -VNetName $VNetName1 `
+           -SubnetName $SubnetName1 `
+           -ResourceGroupName $ResourceGroupName `
+           -Location $Location1 `
+           -Image $Image
+
+#Creating VM in vnet-wus-01 without public IP
+Create-VMs -pipName "" `
+           -EnablePublicIP $false `
+           -Vmname $Vmname3 `
+           -VNetName $VNetName2 `
+           -SubnetName $SubnetName2 `
+           -ResourceGroupName $ResourceGroupName `
+           -Location $Location2 `
+           -Image $Image
+           
+Write-Host "All VMs deployed successfully!"
