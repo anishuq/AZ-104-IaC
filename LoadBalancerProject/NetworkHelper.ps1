4d0670bf9af7f6225c36b4456249fd4a043e9683
unknown <dodulhuq@gmail.com>
2026-01-05 02:08:07 -0500

    more commit

--- LoadBalancerProject/NetworkHelper.ps1
+++ LoadBalancerProject/NetworkHelper.ps1
@@ -1,6 +1,13 @@
-Write-Host "Helper Loaded!"
+Write-Host "Network Helper Loaded!"
 
 
 function New-AzVNetSubnetCreation{
-    Write-Host "Helper Loaded!"
+    param(
+        [string]$vnetName,
+        [string]$subnetName,
+        [string]$addressPrefix
+    )
+    Write-Host "This is a placeholder for New-AzVNetSubnetCreation function. $($vnetName), $($subnetName), $($addressPrefix)    "
+
+    return "Howdee do"
 }
--- LoadBalancerProject/main.ps1
+++ LoadBalancerProject/main.ps1
@@ -3,4 +3,6 @@
 
 $ResourceGroupName = "loadbalancer-rg"
 
-New-AzVNetSubnetCreation
+$ret = New-AzVNetSubnetCreation -vnetName "myVNet" -subnetName "mySubnet" -addressPrefix "10.0.1.0/24"
+
+Write-Host "Returned value from New-AzVNetSubnetCreation: $($ret)"
