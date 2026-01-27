18e9186aefb2951e375e70bb7c95a00e263f0d1a
a_m_anisul_huq <dodulhuq@gmail.com>
2026-01-27 01:43:52 -0500

    App rules created.

--- BastionFWProject/FWHelper.ps1
+++ BastionFWProject/FWHelper.ps1
@@ -36,15 +36,48 @@ function New-AzFWCreation{
     $FWObj = New-AzFirewall -Name $FWName -ResourceGroupName $ResourceGroupName -Location $Location `
             -PublicIpAddress $FWPipObj -VirtualNetwork $vnetObj
 
-    $FWprivateAddress = $FWObj.IpConfigurations.privateipaddress
+    $FWprivateAddress = $FWObj.IpConfigurations[0].privateipaddress
     Write-Host "FW private IP address:   $($FWprivateAddress)"
 
+    # 1. Create the empty container in Azure
     $RTObj = New-AzRouteTable -ResourceGroupName $ResourceGroupName -Location $Location `
                               -Name $RTName -DisableBgpRoutePropagation 
 
+    # 2. Add the route to your LOCAL variable ($RTObj)
+    # Note: I'm capturing the output back into $RTObj to keep it updated
+    $RTObj = Add-AzRouteConfig -Name "dg-to-firewall" `
+                    -AddressPrefix "0.0.0.0/0" `
+                    -NextHopType "VirtualAppliance" `
+                    -NextHopIpAddress $FWprivateAddress `
+                    -RouteTable $RTObj                         
+
+    # 3. Push the local variable's data up to the actual Azure Resource
+    Set-AzRouteTable -RouteTable $RTObj 
+    #$RTObj | Set-AzRouteTable
+
+    # 4. Now that the table actually HAS the route, attach it to the Subnet
     Set-AzVirtualNetworkSubnetConfig -VirtualNetwork $vnetObj -Name $vnetObj.Subnets[0].Name `
                                      -AddressPrefix $vnetObj.Subnets[0].AddressPrefix `
                                      -RouteTable $RTObj
 
-    $vnetObj | Set-AzVirtualNetwork
+    # 5. Push the VNet changes to Azure                                    
+    Set-AzVirtualNetwork -VirtualNetwork $vnetObj 
+    #$vnetObj | Set-AzVirtualNetwork
+
+
+    $appRule01 = New-AzFirewallApplicationRule -Name "Allow-Google-Access" `
+                                                -TargetFqdn "www.google.com" -Protocol "http:80", "https:443" `
+                                                -SourceAddress $vnetObj.Subnets[0].AddressPrefix
+
+    $appRuleCollection = New-AzFirewallApplicationRuleCollection -Name "AppRuleCollection" -Priority 100 `
+                                                                -Rule $appRule01 -ActionType Allow
+
+    $FWObj.ApplicationRuleCollections.Add($appRuleCollection)
+
+    Set-AzFirewall -AzureFirewall $FWObj
+
+
+    #Now we are going to create DNS rule for the VM
+    $DNSnetRule01 = New-AzFirewallNetworkRule -Name "AllowDNS" -SourceAddress $vnetObj.Subnets[0].AddressPrefix `
+                                            
 }
