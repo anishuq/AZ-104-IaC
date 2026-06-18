7bb5129be484e758cb81683f1416a206364911c5
A M ANISUL HUQ <dodulhuq@gmail.com>
2026-06-18 04:31:21 -0400

    setting up auto scale

--- AppServiceProject/main.ps1
+++ AppServiceProject/main.ps1
@@ -18,9 +18,25 @@ Select-AzSubscription -SubscriptionId $SubscriptionId
 New-AzResourceGroup -Name $ResourceGroupName -Location $Location
 
 #Create App Service Plan
-New-AzAppServicePlan -Name $AppServicePlanName -ResourceGroupName $ResourceGroupName `
+$appServicePlan = New-AzAppServicePlan -Name $AppServicePlanName -ResourceGroupName $ResourceGroupName `
 -Location $Location -Tier "Standard" -WorkerSize "Small" -NumberofWorkers 1 -Linux
 
 #Create an web app in the App Service Plan
 New-AzWebApp -Name $WebAppName -ResourceGroupName $ResourceGroupName `
 -Location $Location -AppServicePlan $AppServicePlanName 
+
+$scalerule = New-AzAutoscaleScaleRuleObject -MetricTriggerMetricName "CpuPercentage" `
+            -MetricTriggerOperator "GreaterThan" -MetricTriggerStatistic "Average" `
+            -MetricTriggerThreshold 70 -MetricTriggerTimeGrain 00:01:00 `
+            -MetricTriggerTimeWindow 00:010:00 -MetricTriggerTimeAggregation "Average" `
+            -MetricTriggerResourceId $appServicePlan.Id `
+            -ScaleActionDirection "Increase" -ScaleActionCooldown 00:10:00 `
+            -ScaleActionType "ChangeCount" -ScaleActionValue 1
+               
+
+$autoScaleProfile = New-AzAutoscaleProfileObject -Name "AutoScaleProfile" `
+                     -CapacityDefault "1" -CapacityMaximum "2" -CapacityMinimum "1" `
+                     -Rule $scalerule
+
+New-AzAutoscaleSetting -Name "DeploymentSetting" -ResourceGroupName $ResourceGroupName `
+-Location $Location -TargetResourceId $appServicePlan.Id -AutoscaleProfile $autoScaleProfile -Enabled $true
